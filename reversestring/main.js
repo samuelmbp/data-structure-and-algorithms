0a95df35294228a6128ff68fbeecc62566728b85
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-14 12:08:08 +0100

    Add solution #2 for reversing a string

--- reversestring/main.js
+++ reversestring/main.js
@@ -1,13 +1,22 @@
 // --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
+// Given a string, return a new string with the reversed order of characters
 // --- Examples
 //   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-	return str.split('').reverse().join('').trim();
+	// Solution #1
+	// return str.split('').reverse().join('').trim();
+
+	// Solution #2
+	let reversed = '';
+	for (let char of str) {
+		reversed = char + reversed; // Z-A
+	}
+
+	return reversed.trim();
 }
 
+console.log(reverse('apple'));
 module.exports = reverse;
