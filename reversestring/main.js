fb1f8d1d0e145d98fa193fac052c5d823a7fe170
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-14 11:23:34 +0100

    Improve code readability for reversing a string

--- reversestring/main.js
+++ reversestring/main.js
@@ -2,16 +2,12 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-	const splitString = str.split('');
-  const reverseString = splitString.reverse().join('');
-	return reverseString.trim();
+	return str.split('').reverse().join('').trim();
 }
-
-console.log(reverse('sam')); 
 
 module.exports = reverse;
