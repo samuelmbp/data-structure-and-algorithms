98a8e7f4c8ea4b29587a83c9fb917ac1d3d8a885
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-14 11:18:52 +0100

    Pass all the tests to reverse a string

--- reversestring/main.js
+++ reversestring/main.js
@@ -7,7 +7,11 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  
+	const splitString = str.split('');
+  const reverseString = splitString.reverse().join('');
+	return reverseString.trim();
 }
+
+console.log(reverse('sam')); 
 
 module.exports = reverse;
