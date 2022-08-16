cdfca031432bd72b10612b293e1aedcbd01d798d
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-16 08:07:09 +0100

    Add solution #3 to reverse a string

--- reversestring/main.js
+++ reversestring/main.js
@@ -6,17 +6,22 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-	// Solution #1
+	// --- Solution #1
 	// return str.split('').reverse().join('').trim();
+	
+	// --- Solution #2
+	// let reversed = '';
+	// for (let char of str) {
+	// 	reversed = char + reversed; // Z-A
+	// }
+	// return reversed.trim();
 
-	// Solution #2
-	let reversed = '';
-	for (let char of str) {
-		reversed = char + reversed; // Z-A
-	}
-
-	return reversed.trim();
+	// --- Solution #3
+	return str
+		.split('')
+		.reduce((prev, curr) => curr + prev, '')
+		.trim();
 }
 
-console.log(reverse('apple'));
+console.log(reverse('undefined')); // denifednu
 module.exports = reverse;
