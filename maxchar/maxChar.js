2b30d54f6f374111098f78f07d26b7c04fe6c913
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-24 19:47:22 +0100

    Refactor code to find the max char in a string

--- maxchar/maxChar.js
+++ maxchar/maxChar.js
@@ -10,27 +10,18 @@ const maxChar = (word) => {
 	 * 1. create an empty obj
 	 * 2. loop over string
 	 * 3. check to see if the letter is already in the obj if not add it
-	 * 4. use object.keys and reduce to check the idx of each value
-	 * 5. return the most common string
+	 * 4. use object.keys and reduce to compare the value at each idx 
+	 * 5. return the most common character in the string
 	 */
 	const chars = {};
-
 	for (const char of word) {
 		if (!chars[char]) chars[char] = 1;
 		else chars[char]++;
 	}
 
-	const mostCommonChar = Object.keys(chars).reduce((prev, curr) => {
-		if (chars[prev] > chars[curr]) {
-			return prev;
-		} else {
-			return curr;
-		}
+	return Object.keys(chars).reduce((prev, curr) => {
+		return chars[prev] > chars[curr] ? prev : curr;
 	});
-
-	return mostCommonChar;
 };
-
-maxChar('aabbcccc'); // c
 
 module.exports = maxChar;
