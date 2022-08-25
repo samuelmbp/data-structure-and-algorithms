9a5e475a22cd179abfe77ce6ab5a49692bdd37d6
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-25 07:57:50 +0100

    Fizzbuzz: solution #1

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,3 +1,38 @@
-const fizzBuzz = (number) => {};
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+const fizzBuzz = (number) => {
+	/**
+	 * 1. Is the number multiple of 3 and 5 ? return 'fizzbuzz'
+	 * 2. Is the number multiple of 3? return 'fizz;
+	 * 3. Is the number multiple of 5? return 'buzz'
+	 * 4. If the number is not multiple of 3 or 5 return the number itself
+	 */
+
+	// Return
+	if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz';
+	else if (number % 3 === 0) return 'fizz';
+	else if (number % 5 === 0) return 'buzz';
+	else return number;
+
+	// Console.log
+	// for (let num = 1; num <= number; num++) {
+	// 	if (num % 3 === 0 && num % 5 === 0) console.log('fizzbuzz');
+	// 	else if (num % 3 === 0) console.log('fizz');
+	// 	else if (num % 5 === 0) console.log('buzz');
+	// 	else console.log(num);
+	// }
+};
 
 module.exports = fizzBuzz;
--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -9,15 +9,23 @@ describe('#fizzBuzz', () => {
 	test('should return "fizz" when the number is divisible by 3', () => {
 		expect(fizzBuzz(3)).toBe('fizz');
 		expect(fizzBuzz(12)).toBe('fizz');
+		expect(fizzBuzz(27)).toBe('fizz');
 	});
 
 	test('should return "buzz" when the number is divisible by 5', () => {
 		expect(fizzBuzz(5)).toBe('buzz');
 		expect(fizzBuzz(20)).toBe('buzz');
+		expect(fizzBuzz(70)).toBe('buzz');
 	});
 
 	test('should return "fizzbuzz" when the number is divisible by both: 3 and 5', () => {
 		expect(fizzBuzz(15)).toBe('fizzbuzz');
 		expect(fizzBuzz(30)).toBe('fizzbuzz');
+		expect(fizzBuzz(90)).toBe('fizzbuzz');
+	});
+
+	test('should return the number itself when is not divisible by 3 or 5', () => {
+		expect(fizzBuzz(4)).toBe(4);
+		expect(fizzBuzz(7)).toBe(7);
 	});
 });
