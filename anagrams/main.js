8e2f1ab28bbcfb36e95832dcc0f5ecae148b60f7
Samuel Raducan <samuel.thembp@gmail.com>
2022-09-12 18:23:55 +0100

    Add solution #1 for anagrams exercise

--- anagrams/main.js
+++ anagrams/main.js
@@ -9,10 +9,37 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-	// RegExp -> word.replace(/[^\w]/g, "").toLowerCase();
-	// count the numbers of keys in str1 present in an obj {} and compare with numbers of keys in str2 obj {}
-	// count the length of chars in an str obj? and compare to the length of the other str obj?
-	return stringA.length === stringB.length;
+	/**
+	 * 1. create 2 constants for stringA and stringB with regexp
+	 * 2. create 2 empty objects to store the letter and the count
+	 * 3. iterate over the regexp strings and add them to the objects
+	 * 4. check if the count of obj keys are not equal and return false
+	 * 5. iterate over one obj and compare each char to the other obj
+	 */
+	const wordOne = stringA.replace(/[^\w]/g, '').toLowerCase();
+	const wordTwo = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+	const charsWordOne = {};
+	const charsWordTwo = {};
+
+	for (let char of wordOne) {
+		if (!charsWordOne[char]) charsWordOne[char] = 1;
+		else charsWordOne[char]++;
+	}
+
+	for (let char of wordTwo) {
+		if (!charsWordTwo[char]) charsWordTwo[char] = 1;
+		else charsWordTwo[char]++;
+	}
+
+	if (Object.keys(charsWordOne).length !== Object.keys(charsWordTwo).length) {
+		return false;
+	}
+
+	for (let char in charsWordOne)
+		return charsWordOne[char] === charsWordTwo[char];
 }
+
+console.log(anagrams('sam!!', 'ila'));
 
 module.exports = anagrams;
