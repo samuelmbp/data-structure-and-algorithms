b38c73b713f3898b5fbacdba9b1ab786b1b43ca1
Samuel Raducan <samuel.thembp@gmail.com>
2022-09-13 15:08:01 +0100

    Add first test to check the existence of the function #capitalise

---
+++ capitalise/main.js
@@ -0,0 +1,5 @@
+const capitalise = (str) => {
+
+};
+
+module.exports = capitalise;
---
+++ capitalise/main.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const capitalise = require('./main');
+
+describe('#capitalise', () => {
+	test('should be a function', () => {
+		expect(typeof capitalise).toEqual('function');
+	});
+});
