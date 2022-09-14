631bc2508ab92aabf57a6f761b262f915623e2e6
Samuel Raducan <samuel.thembp@gmail.com>
2022-09-14 15:18:30 +0100

    Add tests to check if the first letter of each word in a string is capitalised

--- capitalise/main.js
+++ capitalise/main.js
@@ -1,5 +1,12 @@
-const capitalise = (str) => {
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-};
+const capitalise = (str) => {};
 
 module.exports = capitalise;
--- capitalise/main.test.js
+++ capitalise/main.test.js
@@ -5,4 +5,14 @@ describe('#capitalise', () => {
 	test('should be a function', () => {
 		expect(typeof capitalise).toEqual('function');
 	});
+
+	test('should capitalise the first letter of every word in a sentence', () => {
+		expect(capitalise('hi there, how is it going?')).toEqual(
+			'Hi There, How Is It Going?'
+		);
+	});
+
+	test('should capitalise the first letter', () => {
+		expect(capitalise('i love breakfast')).toEqual('I Love Breakfast');
+	});
 });
