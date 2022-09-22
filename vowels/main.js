9a700b7e2af91f6592c93e34ce108e5f5547e17d
Samuel Raducan <samuel.thembp@gmail.com>
2022-09-22 09:26:50 +0100

    Add description and remaining tests for the vowels exercise

--- vowels/main.js
+++ vowels/main.js
@@ -1,3 +1,14 @@
-const vowels = (str) => {};
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+const vowels = (str) => {
+
+};
 
 module.exports = vowels;
--- vowels/main.test.js
+++ vowels/main.test.js
@@ -5,4 +5,16 @@ describe('#vowels', () => {
 	test('should contain the vowels function', () => {
 		expect(typeof vowels).toEqual('function');
 	});
+
+	test('should return the number of vowels used', () => {
+		expect(vowels('aeiou')).toBe(5);
+	});
+
+	test('should return the number of vowels used', () => {
+		expect(vowels('abcdefg')).toBe(2);
+	});
+
+	test('should return the number of vowels used', () => {
+		expect(vowels('bcd')).toBe(0);
+	});
 });
