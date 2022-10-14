f05d939c61fad401be24f06626976df6d733250e
Samuel Raducan <samuel.thembp@gmail.com>
2022-10-14 15:32:22 +0100

    Can modify the highest value in the soundWave array

--- band-pass-filter/musicFilter.js
+++ band-pass-filter/musicFilter.js
@@ -6,16 +6,32 @@
  * - Lowest value allowed: is an integer
  * - Highest value allowed: integer
  *
- * Example Input -> Output:
- * [10, 45, 100, 1001], 40, 1000	 |		[40, 45, 100, 1000]
- * [10, 20, 50], 10, 50				   |		[10, 20, 50] (The exact output)
- * [10, 20, 50], 10, 40					 | 		[10, 20, 40]
- * [10, 20, 40], 20, 40					 |		[20, 20, 40]
- * [10, 20, 40], 15, 30					 |		[15, 20, 30]
+ * Example Input: 											-> Output:
+ * [10, 45, 100, 1001], 40, 1000 |		[40, 45, 100, 1000]
+ * [10, 20, 50], 10, 50				   |		[10, 20, 50] -> The exact output - original)
+ * [10, 20, 50], 10, 40					 | 		[10, 20, 40] -> Modifies the highest
+ * [10, 20, 40], 20, 40					 |		[20, 20, 40] -> Modifies the lowest
+ * [10, 20, 40], 15, 30					 |		[15, 20, 30] -> Modifies both
  */
 
 const musicFilter = (soundWave, lowestValue, highestValue) => {
-	return soundWave;
+	/**
+	 * Check if the soundWave contains any value higher than the highest value and modify it
+	 */
+
+	const modifiedSoundWave = [];
+
+	for (let frequency of soundWave) {
+		if (frequency > highestValue) {
+			modifiedSoundWave.push(highestValue);
+		} else {
+			modifiedSoundWave.push(frequency);
+		}
+	}
+
+	return modifiedSoundWave;
 };
+
+console.log(musicFilter([10, 20, 50], 10, 40));
 
 module.exports = musicFilter;
--- band-pass-filter/test.js
+++ band-pass-filter/test.js
@@ -5,4 +5,8 @@ describe('#musicFilter', () => {
 	test('should return the exact sound wave when lowest and highest are the same', () => {
 		expect(musicFilter([10, 20, 50], 10, 50)).toEqual([10, 20, 50]);
 	});
+
+	test('should modify the highest value in the sound wave', () => {
+		expect(musicFilter([10, 20, 50], 10, 40)).toEqual([10, 20, 40]);
+	});
 });
